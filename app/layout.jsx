8d361a16a0b69074aab1fdead3da0c98e8df988a
Virtue Engine <101955083+virtuedevelopment@users.noreply.github.com>
2024-05-16 17:11:24 -0400

    updated input mechanics

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,6 +20,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <CheckToken />
       <AuthProvider>
         <body className={outfit.className}>
