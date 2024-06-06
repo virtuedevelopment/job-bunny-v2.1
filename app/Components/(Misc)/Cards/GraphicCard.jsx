dbd9bee4f0a4637ebe007bb36b70fcdaaa352ebf
Virtue Engine <101955083+virtuedevelopment@users.noreply.github.com>
2024-06-06 00:17:30 -0400

    made changes to graphic card removing the images

--- app/Components/(Misc)/Cards/GraphicCard.jsx
+++ app/Components/(Misc)/Cards/GraphicCard.jsx
@@ -19,9 +19,9 @@ export default function GraphicCard({ graphic, title, message }) {
 
   return (
     <div className={styles.graphicCard}>
-      <div className={styles.imagebox}>
+      {/* <div className={styles.imagebox}>
         <Image src={graphic} width={150} height={150} alt="graphic" />
-      </div>
+      </div> */}
       <div className={styles.infobox}>
         <h2>{title}</h2>
         <p>{message}</p>
